e98040f792ef70900eb686e2ecc074068a52f014
eihi <etigele@gmail.com>
2014-12-17 14:18:01 +0100

    BuildingCard GetValue()

--- Machiavelli/Server/BuildingCard.cpp
+++ Machiavelli/Server/BuildingCard.cpp
@@ -3,12 +3,12 @@
 
 BuildingCard::BuildingCard(int id, string name, int value, int color) : Card(id, name, color)
 {
-
+	_value = value;
 }
 
 BuildingCard::BuildingCard(int id, string name, int value, int color, vector<string> description) : Card(id, name, color, description)
 {
-
+	_value = value;
 }
 
 BuildingCard::~BuildingCard()
--- Machiavelli/Server/BuildingCard.h
+++ Machiavelli/Server/BuildingCard.h
@@ -7,5 +7,10 @@ public:
 	BuildingCard(int id, string name, int value, int color);
 	BuildingCard(int id, string name, int value, int color, vector<string> description);
 	~BuildingCard();
+
+	int GetValue() { return _value; }
+
+private:
+	int _value;
 };
 
